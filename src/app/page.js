bad00384b64f789a099bb5b3976abbd678fe040f
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-08 23:21:29 +0800

    create skills section to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -21,6 +21,24 @@ function Hero() {
   )
 }
 
+function Skill() {
+  return (
+    <section className="flex px-60 items-center my-40">
+      <h2 className="mr-40">My Skills</h2>
+      <ul className="flex gap-x-4">
+        <li className="border-2 bg-gray-400 rounded-md py-2 px-6">HTML5</li>
+        <li className="border-2 bg-gray-400 rounded-md py-2 px-6">CSS3</li>
+        <li className="border-2 bg-gray-400 rounded-md py-2 px-6">Bootstrap</li>
+        <li className="border-2 bg-gray-400 rounded-md py-2 px-6">Tailwind CSS</li>
+        <li className="border-2 bg-gray-400 rounded-md py-2 px-6">JavaScript</li>
+        <li className="border-2 bg-gray-400 rounded-md py-2 px-6">React</li>
+        <li className="border-2 bg-gray-400 rounded-md py-2 px-6">Next.js</li>
+        <li className="border-2 bg-gray-400 rounded-md py-2 px-6">Rails</li>
+      </ul>
+    </section>
+  )
+}
+
 function Footer() {
   return (
     <div className="px-60 flex justify-between">
@@ -38,6 +56,7 @@ export default function Home() {
   return (
     <div>
       <Hero />
+      <Skill />
       <Footer />
     </div>
   );
