ce84e196c76287d973b7bca8ef3aed5aa84f7c47
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-09 23:42:07 +0800

    add styles and responsiveness to hero component

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -2,19 +2,31 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="flex px-60 mt-20">
-      <section className="mr-40 my-auto">
-        <h1 className="text-5xl mb-10">Hi, I&apos;m Zhi Cong</h1>
-        <p>An <em><strong>Engineer</strong></em> embracing a new chapter in <em><strong>Web Development</strong></em>, excited to leverage problem-solving skills and a keen eye for design to build impactful web solutions.</p>
+    <>
+      <section className="md:hidden text-center my-20">
+        <Image
+          src="/Me.jpg"
+          width={180}
+          height={180}
+          className="rounded-full aspect-square object-cover mx-auto"
+          alt="Image of creator"
+        />
+        <h1 className="text-5xl my-4 text-2xl font-semibold">Hello, I&apos;m <span className="text-nowrap">Zhi Cong</span></h1>
+        <p>A <em><strong>Mechanical Engineer</strong></em> embracing a new chapter in <em><strong>Web Development</strong></em>, excited to leverage problem-solving skills and a keen eye for design to build impactful web solutions.</p>
       </section>
-      <Image
-        src="/Me.jpg"
-        width={400}
-        height={400}
-        className="rounded-full"
-        alt="Image of creator"
-        style={{objectFit: "contain"}}
-      />
-    </div>
+      <div className="hidden md:flex mt-20">
+        <section className="mr-10 my-auto">
+          <h1 className="mb-4 text-4xl font-semibold">Hello, I&apos;m Zhi Cong</h1>
+          <p>A <em><strong>Mechanical Engineer</strong></em> embracing a new chapter in <em><strong>Web Development</strong></em>, excited to leverage problem-solving skills and a keen eye for design to build impactful web solutions.</p>
+        </section>
+        <Image
+          src="/Me.jpg"
+          width={300}
+          height={300}
+          className="rounded-full aspect-square object-cover"
+          alt="Image of creator"
+        />
+      </div>
+    </>
   )
 }
