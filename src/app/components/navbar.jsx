f7ef08ec66c7ff634fffbc9a692f27572371ba15
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-09 17:14:04 +0800

    increase font weight of navigation links and add border radius to hamburger icon

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -19,26 +19,26 @@ export default function Navbar() {
     <nav className="px-6 md:px-28 xl:px-96 bg-neutral-300 h-28 w-full">
       <div className="flex flex-row justify-between h-full items-center">
         <Link className={`${lobster.className} text-3xl`} href="/">ZHICONG</Link>
-        <button className="md:hidden" onClick={toggleMenu}>
+        <button className="md:hidden border p-2 rounded-md border-black" onClick={toggleMenu}>
           <FontAwesomeIcon icon={faBars} size="2xl"/>
         </button>
         <div className="hidden md:block">
-          <Link href="/about" className="mx-6 hover:text-2xl duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none">ABOUT</Link>
-          <Link href="/porfolio" className="mx-6 hover:text-2xl duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none">PORTFOLIO</Link>
-          <Link href="/contact" className="mx-6 hover:text-2xl duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none">CONTACT</Link>
+          <Link href="/about" className="font-bold mx-6 hover:text-2xl duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none">ABOUT</Link>
+          <Link href="/porfolio" className="font-bold font-bold mx-6 hover:text-2xl duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none">PORTFOLIO</Link>
+          <Link href="/contact" className="font-bold mx-6 hover:text-2xl duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none">CONTACT</Link>
         </div>
       </div>
       <div className="flex justify-end md:hidden">
         { isMenuOpen &&
           <ul className="">
-            <li className="my-4">
-              <a href="/about">ABOUT</a>
+            <li className="my-4 font-bold">
+              <Link href="/about">ABOUT</Link>
             </li>
-            <li className="my-4">
-              <a href="/porfolio">PORTFOLIO</a>
+            <li className="my-4 font-bold">
+              <Link href="/porfolio">PORTFOLIO</Link>
             </li>
-            <li className="my-4">
-              <a href="/contact">CONTACT</a>
+            <li className="my-4 font-bold">
+              <Link href="/contact">CONTACT</Link>
             </li>
           </ul>
         }
