edc01af4de3ad89f634686e04fd14a6bc3166144
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-11 16:36:24 +0800

    modify styles of hero component

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <>
-      <section className="md:hidden text-center my-20">
+      <section className="text-center my-20">
         <Image
           src="/Me.jpg"
           width={180}
@@ -11,22 +11,9 @@ export default function Hero() {
           className="rounded-full aspect-square object-cover mx-auto"
           alt="Image of creator"
         />
-        <h1 className="text-5xl my-4 text-2xl font-semibold">Hello, I&apos;m <span className="text-nowrap">Zhi Cong</span></h1>
+        <h1 className="text-5xl mt-10 mb-5 text-2xl font-semibold">Hello, I&apos;m <span className="text-nowrap">Zhi Cong</span></h1>
         <p>A <em><strong>Mechanical Engineer</strong></em> embracing a new chapter in <em><strong>Web Development</strong></em>, excited to leverage problem-solving skills and a keen eye for design to build impactful web solutions.</p>
       </section>
-      <div className="hidden md:flex mt-20">
-        <section className="mr-10 my-auto">
-          <h1 className="mb-4 text-4xl font-semibold">Hello, I&apos;m Zhi Cong</h1>
-          <p>A <em><strong>Mechanical Engineer</strong></em> embracing a new chapter in <em><strong>Web Development</strong></em>, excited to leverage problem-solving skills and a keen eye for design to build impactful web solutions.</p>
-        </section>
-        <Image
-          src="/Me.jpg"
-          width={300}
-          height={300}
-          className="rounded-full aspect-square object-cover"
-          alt="Image of creator"
-        />
-      </div>
     </>
   )
 }
