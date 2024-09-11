2fda237a858bd1fd52c5302f68f8607fe3042854
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-11 12:18:22 +0800

    modify styles, add responsiveness and an image carousel to project card component

--- src/app/components/project-card.jsx
+++ src/app/components/project-card.jsx
@@ -1,18 +1,78 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import Image from "next/image"
+
+const imageList = [
+  { id: 0, src: "/FitBud/Home.png", alt: "Image of FitBud homepage" },
+  { id: 1, src: "/FitBud/Profile.png", alt: "Image of FitBud profile page" },
+  { id: 2, src: "/FitBud/Plan.png", alt: "Image of FitBud plan page" },
+  { id: 3, src: "/FitBud/Workout.png", alt: "Image of FitBud workout page" },
+]
+
+function Carousel({ images }) {
+  const [currentImageId, setCurrentImageId] = useState(0);
+
+  let image = images.find(image => image.id === currentImageId);
+
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      currentImageId === images[images.length - 1].id ?
+      setCurrentImageId(0) : setCurrentImageId(currentImageId + 1)
+    }, 2500);
+
+    return () => clearTimeout(timeOutId);
+  }, [currentImageId, images]);
+
+  function handleLeftArrowClick() {
+    currentImageId === 0 ?
+    setCurrentImageId(images.length - 1) : setCurrentImageId(currentImageId - 1)
+  }
+
+  function handleRightArrowClick() {
+    currentImageId === images.length - 1 ?
+    setCurrentImageId(0) : setCurrentImageId(currentImageId + 1);
+  }
+
+  return (
+    <div>
+      <div className="flex justify-center">
+        <button
+          className="text-2xl text-neutral-500 h-max my-auto"
+          onClick={handleLeftArrowClick}
+        >
+          &lt;
+        </button>
+        <div className="w-44 h-80 md:w-52 md:h-96 relative mx-4">
+          <Image
+            src={image.src}
+            alt={image.alt}
+            fill={true}
+            style={{objectFit: "cover"}}
+          />
+        </div>
+        <button
+          className="text-2xl text-neutral-500 h-max my-auto"
+          onClick={handleRightArrowClick}
+        >
+          &gt;
+        </button>
+      </div>
+      <div className="flex justify-center gap-x-3 mt-1 text-xl">
+        {images.map(image => image.id === currentImageId ?
+          <p key={image.id} className="text-blue-500">●</p> :
+          <p key={image.id} className="text-neutral-200">●</p>
+        )}
+      </div>
+    </div>
+  )
+}
 
 export default function ProjectCard({ project }) {
   return (
-    <section className="md:flex justify-between mt-20">
-      <div className="w-32 h-80 md:w-48 md:h-96 relative mx-auto md:mx-0">
-        <Image
-          src="/istockphoto-1324356458-2048x2048.jpg"
-          alt="Image of project"
-          fill={true}
-        />
-      </div>
-      <div className="md:w-3/5">
+    <section className="md:flex justify-between mt-5 text-center md:text-left">
+      <Carousel images={imageList}/>
+      <div className="md:ml-20">
         <h2 className="my-4 text-xl font-semibold">{project.title}</h2>
         <p>{project.description}</p>
       </div>
