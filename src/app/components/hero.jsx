e441a4f69fb4bcf3c240bce2979db92eccfa0cd5
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-11 17:36:50 +0800

    add priority to image in hero component

--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -10,6 +10,7 @@ export default function Hero() {
           height={180}
           className="rounded-full aspect-square object-cover mx-auto"
           alt="Image of creator"
+          priority={true}
         />
         <h1 className="text-5xl mt-10 mb-5 text-2xl font-semibold">Hello, I&apos;m <span className="text-nowrap">Zhi Cong</span></h1>
         <p>A <em><strong>Mechanical Engineer</strong></em> embracing a new chapter in <em><strong>Web Development</strong></em>, excited to leverage problem-solving skills and a keen eye for design to build impactful web solutions.</p>
