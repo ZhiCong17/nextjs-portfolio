856147ae9aea357aff40551b3869e67d23abafca
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-12 15:29:18 +0800

    add contents into about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,9 +2,17 @@ export default function Page() {
   return (
     <>
       <h1 className="text-center md:text-left mt-20 text-4xl font-semibold">About Me</h1>
-      <p className="text-center mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum animi ex impedit quo, voluptatibus enim totam at, atque a iste quibusdam. Voluptatibus consequuntur pariatur eligendi aliquam ullam quis fuga!</p>
-      <h2 className="text-center md:text-left mt-20 text-4xl font-semibold">Journey</h2>
-      <p className="text-center mt-5 mb-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum animi ex impedit quo, voluptatibus enim totam at, atque a iste quibusdam. Voluptatibus consequuntur pariatur eligendi aliquam ullam quis fuga!</p>
+      <p className="text-center md:text-left mt-5">Hello! I&apos;m Zhi Cong, an inspiring web developer from the vibrant garden city Singapore. Previously worked as a draft and Building Information Modeling (BIM) modeler, where I specialized in creating detailed 2D drawings and 3D models for mechanical services.</p>
+      <p className="text-center md:text-left mt-5">Driven by a passion for technology and creativity, I decided to transit into web development with a focus on front-end technologies. My goal is to craft inspiring and impactful web applications that offer exceptional user experiences.</p>
+      <p className="text-center md:text-left mt-5">When I&apos;m not coding, you&apos;ll likely find me at the gym, jogging around the neighborhood, or indulging in my favorite anime series. I&apos;m always eager to learn new things and take on exciting challenges in the ever-evolving world of web development.</p>
+      <p className="text-center md:text-left mt-5">Feel free to connect with me—whether you&apos;re interested in discussing tech, sharing anime recommendations, or exploring potential collaborations!</p>
+
+      <h2 className="text-center md:text-left md:text-left mt-20 text-4xl font-semibold">My Journey</h2>
+      <p className="text-center md:text-left mt-5">My journey into the tech world began in 2024 when I enrolled in a Web Development course at Le Wagon. During the bootcamp, I immersed myself in various programming tools and technologies, including Visual Studio Code, Git, Ruby, SQL with Active Record, and core web technologies like HTML, CSS, and JavaScript.</p>
+      <p className="text-center md:text-left mt-5">One of the highlights of the bootcamp was working on a team project where we developed FitBud, a web application that generates personalized workout plans for users. This project was a valuable learning experience and was deployed on Heroku, showcasing our ability to build and deploy a functional web application.</p>
+      <p className="text-center md:text-left mt-5">Following the bootcamp, my team and I leveraged our skills to create BitSpend, an expenses tracking application. We enter it into the Gemini API Developer Competition, which provided an opportunity to apply our knowledge in a competitive setting.</p>
+      <p className="text-center md:text-left mt-5">Currently, I am expanding my skill set by learning advanced technologies such as React.js and Next.js. My goal is to continue growing and mastering new tools that will propel my career forward.</p>
+      <p className="text-center md:text-left mt-5 mb-20">Although my journey in tech is just beginning, I’m excited by how much I’ve learned and am eager to keep pushing the boundaries of what I can achieve.</p>
     </>
   )
 }
