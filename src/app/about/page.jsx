f51f62818abab45f54a56558785de4a2a12e4de6
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-12 12:18:09 +0800

    create an about page with placeholder sections

---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-center md:text-left mt-20 text-4xl font-semibold">About Me</h1>
+      <p className="text-center mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum animi ex impedit quo, voluptatibus enim totam at, atque a iste quibusdam. Voluptatibus consequuntur pariatur eligendi aliquam ullam quis fuga!</p>
+      <h2 className="text-center md:text-left mt-20 text-4xl font-semibold">Journey</h2>
+      <p className="text-center mt-5 mb-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum animi ex impedit quo, voluptatibus enim totam at, atque a iste quibusdam. Voluptatibus consequuntur pariatur eligendi aliquam ullam quis fuga!</p>
+    </>
+  )
+}
