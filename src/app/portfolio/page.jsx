76cf7b454b5d4d31023ea4610b9937f696c9d0eb
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-16 22:54:18 +0800

    add structure and placeholder content to projects in portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -4,13 +4,29 @@ import Carousel from "../components/carousel";
 export default function Page() {
   return (
     <section>
-      <h1 className="text-center md:text-left text-4xl font-semibold">My Projects</h1>
-      <h2 className="mt-5 mb-5 text-xl font-semibold text-center">FitBud</h2>
-      <Carousel images={fitBud.images} />
+      <h1 className="text-center md:text-left text-4xl font-semibold mb-5">My Projects</h1>
+      <h2 className="md:hidden mb-5 text-xl font-semibold text-center md:text-center">FitBud</h2>
+      <div className="md:float-right md:ml-20 md:mb-5 md:mt-10">
+        <Carousel images={fitBud.images} />
+      </div>
+      <h2 className="hidden md:block mb-5 text-xl font-semibold text-center md:text-center">FitBud</h2>
+      <p>GitHub: </p>
+      <p>Demo: </p>
       <p className="mt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nobis dignissimos placeat sapiente hic necessitatibus! Sequi quos tenetur dolorum obcaecati nobis corporis omnis recusandae illum, inventore vero. Nostrum, exercitationem ipsum!</p>
       <p className="mt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nobis dignissimos placeat sapiente hic necessitatibus! Sequi quos tenetur dolorum obcaecati nobis corporis omnis recusandae illum, inventore vero. Nostrum, exercitationem ipsum!</p>
-      <h2 className="mt-10 mb-5 text-xl font-semibold text-center">BitSpend</h2>
-      <Carousel images={fitBud.images} />
+      <p className="mt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nobis dignissimos placeat sapiente hic necessitatibus! Sequi quos tenetur dolorum obcaecati nobis corporis omnis recusandae illum, inventore vero. Nostrum, exercitationem ipsum!</p>
+      <p className="mt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nobis dignissimos placeat sapiente hic necessitatibus! Sequi quos tenetur dolorum obcaecati nobis corporis omnis recusandae illum, inventore vero. Nostrum, exercitationem ipsum!</p>
+      <p className="mt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nobis dignissimos placeat sapiente hic necessitatibus! Sequi quos tenetur dolorum obcaecati nobis corporis omnis recusandae illum, inventore vero. Nostrum, exercitationem ipsum!</p>
+      <h2 className="md:hidden mb-5 mt-14 text-xl font-semibold text-center md:text-center">BitSpend</h2>
+      <div className="md:float-right md:ml-20 md:mb-5 md:mt-24">
+        <Carousel images={bitSpend.images} />
+      </div>
+      <h2 className="hidden md:block mb-5 mt-14 text-xl font-semibold text-center md:text-center">BitSpend</h2>
+      <p>GitHub: </p>
+      <p>Demo: </p>
+      <p className="mt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nobis dignissimos placeat sapiente hic necessitatibus! Sequi quos tenetur dolorum obcaecati nobis corporis omnis recusandae illum, inventore vero. Nostrum, exercitationem ipsum!</p>
+      <p className="mt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nobis dignissimos placeat sapiente hic necessitatibus! Sequi quos tenetur dolorum obcaecati nobis corporis omnis recusandae illum, inventore vero. Nostrum, exercitationem ipsum!</p>
+      <p className="mt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nobis dignissimos placeat sapiente hic necessitatibus! Sequi quos tenetur dolorum obcaecati nobis corporis omnis recusandae illum, inventore vero. Nostrum, exercitationem ipsum!</p>
       <p className="mt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nobis dignissimos placeat sapiente hic necessitatibus! Sequi quos tenetur dolorum obcaecati nobis corporis omnis recusandae illum, inventore vero. Nostrum, exercitationem ipsum!</p>
       <p className="mt-5 mb-20">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti nobis dignissimos placeat sapiente hic necessitatibus! Sequi quos tenetur dolorum obcaecati nobis corporis omnis recusandae illum, inventore vero. Nostrum, exercitationem ipsum!</p>
     </section>
