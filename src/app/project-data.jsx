462340a82fa12691b26ed5dc36a608d4d1ae08ae
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-19 18:29:04 +0800

    edit projects description in project data

--- src/app/project-data.jsx
+++ src/app/project-data.jsx
@@ -1,7 +1,7 @@
 export const fitBud = {
   id: 1,
   title: "FitBud",
-  description: "A mobile focused web application which generates gym workout plans for users based on their individual profile to help them achieve their fitness goal.",
+  description: "A mobile web application which generates gym workout plans for users based on their individual profile to help them achieve their fitness goal.",
   images: [
     { id: 0, src: "/FitBud/Home.png", alt: "Image of FitBud homepage" },
     { id: 1, src: "/FitBud/Profile.png", alt: "Image of FitBud profile page" },
@@ -13,7 +13,7 @@ export const fitBud = {
 export const bitSpend = {
   id: 2,
   title: "BitSpend",
-  description: "A mobile focused expense tracking web application which allows users to record and view expenses history to achieve their financial goal.",
+  description: "A mobile expense tracking web application which allows users to record and view expenses history to achieve their financial goal.",
   images: [
     { id: 0, src: "/BitSpend/Landing.png", alt: "Image of BitSpend landing page" },
     { id: 1, src: "/BitSpend/Record.png", alt: "Image of BitSpend home page" },
