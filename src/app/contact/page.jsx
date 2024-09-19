5ea97238abb2948d3eb114ed387ed452f0c317fa
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-19 23:39:54 +0800

    add contact details under contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,7 +3,12 @@ import ContactForm from "../components/contact-form"
 export default function Page() {
   return (
     <section>
-      <h1 className="text-4xl font-semibold">Contact</h1>
+      <h1 className="text-4xl font-semibold">Contact Details</h1>
+      <p className="mt-5">Email: zhicong1989@gmail.com</p>
+      <p className="">GitHub: ZhiCong17</p>
+      <p className="">LinkedIn: Zhi Cong Ore</p>
+
+      <h2 className="text-4xl font-semibold mt-14">Contact Me</h2>
       <p className="my-5">Drop me an email and I will get back to you as soon as I can.</p>
       <ContactForm />
     </section>
