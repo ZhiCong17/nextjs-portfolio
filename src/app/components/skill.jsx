c1f5753a4e880867f11e0e42482fa94a5d70f498
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-20 22:49:50 +0800

    add shadow and increase gap size of list items in skill component

--- src/app/components/skill.jsx
+++ src/app/components/skill.jsx
@@ -2,15 +2,15 @@ export default function Skill() {
   return (
     <section className="md:flex my-14">
       <h2 className="mb-10 md:w-5/12 md:mr-20 text-4xl font-semibold">Tech Stack</h2>
-      <ul className="flex gap-1 flex-wrap justify-start md:justify-start">
-        <li className="bg-neutral-400 rounded-md py-2 px-3">HTML</li>
-        <li className="bg-neutral-400 rounded-md py-2 px-3">CSS</li>
-        <li className="bg-neutral-400 rounded-md py-2 px-3">Tailwind CSS</li>
-        <li className="bg-neutral-400 rounded-md py-2 px-3">Bootstrap</li>
-        <li className="bg-neutral-400 rounded-md py-2 px-3">JavaScript</li>
-        <li className="bg-neutral-400 rounded-md py-2 px-3">React</li>
-        <li className="bg-neutral-400 rounded-md py-2 px-3">Next.js</li>
-        <li className="bg-neutral-400 rounded-md py-2 px-3">Ruby on Rails</li>
+      <ul className="flex gap-2 flex-wrap justify-start md:justify-start">
+        <li className="bg-neutral-400 rounded-md py-2 px-3 shadow-md shadow-neutral-500">HTML</li>
+        <li className="bg-neutral-400 rounded-md py-2 px-3 shadow-md shadow-neutral-500">CSS</li>
+        <li className="bg-neutral-400 rounded-md py-2 px-3 shadow-md shadow-neutral-500">Tailwind CSS</li>
+        <li className="bg-neutral-400 rounded-md py-2 px-3 shadow-md shadow-neutral-500">Bootstrap</li>
+        <li className="bg-neutral-400 rounded-md py-2 px-3 shadow-md shadow-neutral-500">JavaScript</li>
+        <li className="bg-neutral-400 rounded-md py-2 px-3 shadow-md shadow-neutral-500">React</li>
+        <li className="bg-neutral-400 rounded-md py-2 px-3 shadow-md shadow-neutral-500">Next.js</li>
+        <li className="bg-neutral-400 rounded-md py-2 px-3 shadow-md shadow-neutral-500">Ruby on Rails</li>
       </ul>
     </section>
   )
