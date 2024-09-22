8f38b39e7c66cc6721a9ffde1d8c65e49e01ff00
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-22 17:53:24 +0800

    refactor navbar rendering

--- src/app/components/shared/navbar.jsx
+++ src/app/components/shared/navbar.jsx
@@ -15,10 +15,13 @@ const lobster = Lobster({
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  let navLinksClass;
 
   function toggleMenu() {
     setIsMenuOpen(!isMenuOpen);
   }
+
+  isMenuOpen === false ? navLinksClass = "hidden md:flex gap-x-5" : navLinksClass = "absolute right-0 text-right ";
 
   function ToggleThemeButton() {
     const [mounted, setMounted] = useState(false);
@@ -34,12 +37,12 @@ export default function Navbar() {
     };
 
     buttonContent = theme === "light" ?
-    <div className="w-16 h-8 rounded-full bg-background flex justify-between items-center">
+    <div className="w-16 h-8 rounded-full bg-foreground flex justify-between items-center">
       <div className="w-6 h-6 rounded-full bg-lightnavbg ml-1"></div>
-      <FontAwesomeIcon icon={faMoon} className="mr-2 text-foreground" size="xl" />
+      <FontAwesomeIcon icon={faMoon} className="mr-2 text-background" size="xl" />
     </div> :
-    <div className="w-16 h-8 rounded-full bg-darkbg flex justify-between items-center">
-      <FontAwesomeIcon icon={faSun} className="ml-2 text-darktext" size="lg" />
+    <div className="w-16 h-8 rounded-full bg-darktext flex justify-between items-center">
+      <FontAwesomeIcon icon={faSun} className="ml-2 text-darkbg" size="lg" />
       <div className="w-6 h-6 rounded-full bg-darknavbg mr-1"></div>
     </div>
 
@@ -54,19 +57,11 @@ export default function Navbar() {
     <nav className="bg-lightnavbg dark:bg-darknavbg h-20 w-full sticky top-0 z-50">
       <div className="px-6 mx-auto max-w-sm md:max-w-3xl flex flex-row justify-between h-full items-center">
         <Link className={`${lobster.className} text-3xl`} href="/" onClick={() => setIsMenuOpen(false)}>ZHICONG</Link>
-        <button className="md:hidden border p-2 rounded-md border-black" onClick={toggleMenu}>
-          <FontAwesomeIcon icon={faBars} size="2xl"/>
-        </button>
-        <div className="hidden md:flex items-center gap-x-5">
-          <Link href="/about" className="font-bold hover:text-2xl duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none">ABOUT</Link>
-          <Link href="/portfolio" className="font-bold font-bold hover:text-2xl duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none">PORTFOLIO</Link>
-          <Link href="/contact" className="font-bold hover:text-2xl duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none">CONTACT</Link>
-          <ToggleThemeButton />
-        </div>
-      </div>
-      <div className="flex justify-end md:hidden">
-        { isMenuOpen &&
-          <ul className="">
+        <div className="relative text-right">
+          <button className="md:hidden border p-2 rounded-md border-foreground dark:border-darktext" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faBars} size="2xl"/>
+          </button>
+          <ul className={navLinksClass}>
             <li className="my-4 font-bold">
               <Link href="/about" onClick={() => setIsMenuOpen(false)}>ABOUT</Link>
             </li>
@@ -76,8 +71,9 @@ export default function Navbar() {
             <li className="my-4 font-bold">
               <Link href="/contact" onClick={() => setIsMenuOpen(false)}>CONTACT</Link>
             </li>
+            <ToggleThemeButton />
           </ul>
-        }
+        </div>
       </div>
     </nav>
   )
