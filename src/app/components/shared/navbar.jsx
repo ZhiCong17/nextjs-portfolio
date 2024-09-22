45171ef8deeb485d537aef33ab935cd505ca6001
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-22 23:26:31 +0800

    move toggle theme button to a single file under components

--- src/app/components/shared/navbar.jsx
+++ src/app/components/shared/navbar.jsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useTheme } from "next-themes";
 import Link from "next/link";
 import { Lobster } from "next/font/google";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import ToggleThemeButton from "../toggle-theme-button";
 
 const lobster = Lobster({
   weight: "400",
@@ -22,36 +22,6 @@ export default function Navbar() {
   }
 
   isMenuOpen === false ? navLinksClass = "hidden md:flex gap-x-5" : navLinksClass = "absolute right-0 text-right ";
-
-  function ToggleThemeButton() {
-    const [mounted, setMounted] = useState(false);
-    const { theme, setTheme } = useTheme();
-    let buttonContent;
-
-    useEffect(() => {
-      setMounted(true);
-    }, []);
-
-    if (!mounted) {
-      return null;
-    };
-
-    buttonContent = theme === "light" ?
-    <div className="w-16 h-8 rounded-full bg-foreground flex justify-between items-center">
-      <div className="w-6 h-6 rounded-full bg-lightnavbg ml-1"></div>
-      <FontAwesomeIcon icon={faMoon} className="mr-2 text-background" size="xl" />
-    </div> :
-    <div className="w-16 h-8 rounded-full bg-darktext flex justify-between items-center">
-      <FontAwesomeIcon icon={faSun} className="ml-2 text-darkbg" size="lg" />
-      <div className="w-6 h-6 rounded-full bg-darknavbg mr-1"></div>
-    </div>
-
-    return (
-      <button onClick={() => {theme === "dark" ? setTheme("light") : setTheme("dark")}}>
-        {buttonContent}
-      </button>
-    )
-  }
 
   return (
     <nav className="bg-lightnavbg dark:bg-darknavbg h-20 w-full sticky top-0 z-50">
---
+++ src/app/components/toggle-theme-button.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+
+export default function ToggleThemeButton() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+  let buttonContent;
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  };
+
+  buttonContent = theme === "light" ?
+  <div className="w-16 h-8 rounded-full bg-foreground flex justify-between items-center">
+    <div className="w-6 h-6 rounded-full bg-lightnavbg ml-1"></div>
+    <FontAwesomeIcon icon={faMoon} className="mr-2 text-background" size="xl" />
+  </div> :
+  <div className="w-16 h-8 rounded-full bg-darktext flex justify-between items-center">
+    <FontAwesomeIcon icon={faSun} className="ml-2 text-darkbg" size="lg" />
+    <div className="w-6 h-6 rounded-full bg-darknavbg mr-1"></div>
+  </div>
+
+  return (
+    <button onClick={() => {theme === "dark" ? setTheme("light") : setTheme("dark")}}>
+      {buttonContent}
+    </button>
+  )
+}
