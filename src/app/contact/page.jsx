33eb678783284e1cdb0b0374e2aabd678f81c0a9
ZhiCong Ore <zhicong1989@gmail.com>
2024-09-25 15:34:25 +0800

    update linkedin user in contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,7 +6,7 @@ export default function Page() {
       <h1 className="text-4xl font-semibold">Contact Details</h1>
       <p className="mt-5">Email: zhicong1989@gmail.com</p>
       <p className="">GitHub: ZhiCong17</p>
-      <p className="">LinkedIn: Zhi Cong Ore</p>
+      <p className="">LinkedIn: zhicong-ore</p>
 
       <h2 className="text-4xl font-semibold mt-14">Contact Me</h2>
       <p className="my-5">Drop me an email and I will get back to you as soon as I can.</p>
